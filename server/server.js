81f2e7cb726242fa0b2b064731a8ed75e22dee5c
GodFather93 <suryapalsingh.inda@gmail.com>
2018-07-16 17:02:06 +0530

    add get with params

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -42,6 +43,25 @@ app.get('/todo',(req,res)=>{
   },(e)=>{
     res.status(400).send(e);
   });
+});
+
+//with params
+
+app.get('/todo/:id',(req,res)=>{
+var id = req.params.id;
+if(!ObjectId.isValid(id)){
+  return res.status(404).send();
+}
+Todo.findById(id).then((todo) =>{
+  if(!todo){
+    return res.status(404).send();
+  }
+  res.send(todo);
+
+}).catch((e)=>{
+  res.send(400).send();
+})
+
 });
 
 app.listen(3000,() =>{
